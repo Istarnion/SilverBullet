dbc09702427376ee21fa8f853671a421fbfc071b
Hallgeir Løkken <istarnion@gmail.com>
2014-11-01 12:36:25 +0100

    Update

--- SilverBullet/Assets/Scripts/PlayerMovementScript.cs
+++ SilverBullet/Assets/Scripts/PlayerMovementScript.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovementScript : MonoBehaviour {
+public class PlayerMovementScript : MonoBehaviour
+{
+
+	public float speed = 4.0f;
+	CharacterController cc;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+		Vector3 targetDirection = new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+		targetDirection.Normalize();
+		targetDirection *= Time.deltaTime;
+		cc.Move(targetDirection * speed);
 	}
 }
